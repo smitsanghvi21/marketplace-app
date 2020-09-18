d4cbde65162a23b77cd0c334986302f8baf67969
smitsanghvi21 <sanghvismit02@gmail.com>
2020-09-18 22:24:33 +1200

    new screen

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Welcome from './app/screens/Welcome';
+import ViewImage from './app/screens/ViewImage';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Welcome />
+      <ViewImage />
     </View>
   );
 }
